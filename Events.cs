d1568578cc691a5a8493ccdee7b4ade85b99dda8
anshu-theweirdo <141988759+anshu-theweirdo@users.noreply.github.com>
2023-09-16 07:26:42 +0530

    Add files via upload
    An Endless Runner Game from scratch with blender and Unity 3D.

---
+++ Events.cs
@@ -0,0 +1,17 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Events : MonoBehaviour
+{
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void QuiteGame()
+    {
+        Application.Quit();
+    }
+
+
+}
